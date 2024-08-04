c4e6477bf3e5716a62ff713488fba106fb6c3e41
Felipe Nascimento <felipe@on2.dev>
2024-08-04 00:37:19 -0300

    deploying to gh-pages

--- _next/static/UNSM8G-5fS_alaCUWY9_k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a6a91d0c36d25ca1.css","static/chunks/pages/index-fcac679edd97dbf8.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

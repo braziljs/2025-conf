fa6134f5742c63ea8e136b9f81e670ec392aa265
Felipe Nascimento <felipe@on2.dev>
2024-08-05 18:19:35 -0300

    deploying to gh-pages

--- dist/_next/static/0D77mzmKix4AvHSUeFrlp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/df0ded876b29b7b9.css","static/chunks/pages/index-41af7907bad46435.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ dist/_next/static/rsXV5BmkOH42hYVgXDjbz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/fffaec6ee8f2b8a3.css","static/chunks/pages/index-4a1b5a4820770f29.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
